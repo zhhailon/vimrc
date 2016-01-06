5dd9c2aff009f1c89ed0095d530e8e61237ab5ad
rkk <zhhailon@gmail.com>
2016-01-06 09:57:08 -0500

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -3,20 +3,21 @@ git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 mkdir ~/.vim_runtime/sources_plugins
 git clone https://github.com/lervag/vimtex.git ~/.vim_runtime/sources_plugins/vimtex
 echo "set number
-set tw=79
 call pathogen#infect('~/.vim_runtime/sources_plugins/{}')
 set gfn=Fantasque\ Sans\ Mono\ 12" > ~/.vim_runtime/my_configs.vim
 echo "<<<<<< Install vimrc"
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 echo "
-set shiftwidth=2
-set tabstop=2
 if ! has(\"gui_running\")
   set t_Co=256
 endif
 \" feel free to choose :set background=light for a different style
 set background=dark
 colors peaksea
+
+set shiftwidth=2
+set tabstop=2
+set tw=79
 
 map <leader>tt :tabnext<cr>" >> ~/.vimrc
 
