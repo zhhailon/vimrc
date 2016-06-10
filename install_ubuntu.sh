2a0b0afd831d6dfe9260fb71bb35aea47cfc72b9
rkk <zhhailon@gmail.com>
2016-06-10 16:58:50 -0400

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,7 +1,7 @@
 echo "<<<<<< Clone amix/vimrc"
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
-mkdir ~/.vim_runtime/sources_plugins
-git clone https://github.com/lervag/vimtex.git ~/.vim_runtime/sources_plugins/vimtex
+# mkdir ~/.vim_runtime/sources_plugins
+# git clone https://github.com/lervag/vimtex.git ~/.vim_runtime/sources_plugins/vimtex
 echo "set number
 call pathogen#infect('~/.vim_runtime/sources_plugins/{}')
 set gfn=Fantasque\ Sans\ Mono\ 12" > ~/.vim_runtime/my_configs.vim
