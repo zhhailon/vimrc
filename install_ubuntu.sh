27afb1e1457e84db75bb54061591254df5424197
rkk <zhhailon@gmail.com>
2017-07-17 14:06:50 -0700

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -5,18 +5,16 @@ git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 # echo "" > ~/.vim_runtime/my_configs.vim
 echo "<<<<<< Install vimrc"
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-echo "
-set number
+echo "set number
 set shiftwidth=2
 set tabstop=2
-set tw=69
-map <leader>tt :tabnext<cr>" >> ~/.vimrc
+set tw=69" >> ~/.vimrc
 
 # sudo apt-get install exuberant-ctags
 
 echo "<<<<<< Install TagBar"
 git clone https://github.com/majutsushi/tagbar.git ~/.vim_runtime/sources_non_forked/tagbar
-echo "nmap <F8> :TagbarToggle<CR>" > ~/.vim_runtime/my_configs.vim
+echo "nmap <F8> :TagbarToggle<CR>" >> ~/.vim_runtime/my_configs.vim
 
 echo "<<<<<< Install YouCompleteMe"
 git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim_runtime/sources_non_forked/YouCompleteMe
