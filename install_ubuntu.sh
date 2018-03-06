acf3fc7b786f61224ebf7a7c38aab84eca7ea523
rkk <zhhailon@gmail.com>
2018-03-06 11:50:39 -0500

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -20,6 +20,7 @@ echo "<<<<<< Install YouCompleteMe"
 git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim_runtime/sources_non_forked/YouCompleteMe
 cd ~/.vim_runtime/sources_non_forked/YouCompleteMe
 git submodule update --init --recursive
+export PYTHON_CONFIGURE_OPTS="--enable-shared"
 ./install.py --clang-completer
 echo "let g:ycm_global_ycm_extra_conf = '~/.vim_runtime/sources_non_forked/YouCompleteMe/third_party/ycmd/cpp/.ycm_extra_conf.py'" >> ~/.vim_runtime/my_configs.vim
 
