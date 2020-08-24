2f5d928437207042fc01e40dbf7a293de83d19f8
zhhailon <653602+zhhailon@users.noreply.github.com>
2020-08-24 17:00:43 -0400

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,27 +1,15 @@
-echo "<<<<<< Clone amix/vimrc"
-git clone https://github.com/amix/vimrc.git ~/.vim_runtime
-# mkdir ~/.vim_runtime/sources_plugins
-# git clone https://github.com/lervag/vimtex.git ~/.vim_runtime/sources_plugins/vimtex
-# echo "" > ~/.vim_runtime/my_configs.vim
-echo "<<<<<< Install vimrc"
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 echo "set number
 set shiftwidth=2
 set tabstop=2
-set tw=69" >> ~/.vimrc
+set tw=79" >> ~/.vim_runtime/my_configs.vim
 
-# sudo apt-get install exuberant-ctags
-
-echo "<<<<<< Install TagBar"
-git clone https://github.com/majutsushi/tagbar.git ~/.vim_runtime/sources_non_forked/tagbar
+git clone https://github.com/majutsushi/tagbar.git ~/.vim_runtime/my_plugins/tagbar
 echo "nmap <F8> :TagbarToggle<CR>" >> ~/.vim_runtime/my_configs.vim
 
-echo "<<<<<< Install YouCompleteMe"
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim_runtime/sources_non_forked/YouCompleteMe
-cd ~/.vim_runtime/sources_non_forked/YouCompleteMe
-git submodule update --init --recursive
-# CONFIGURE_OPTS=--enable-shared pyenv install 3.6.4
-./install.py --clang-completer
-echo "let g:ycm_global_ycm_extra_conf = '~/.vim_runtime/sources_non_forked/YouCompleteMe/third_party/ycmd/cpp/.ycm_extra_conf.py'" >> ~/.vim_runtime/my_configs.vim
+git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim_runtime/my_plugins/YouCompleteMe
+cd ~/.vim_runtime/my_plugins/YouCompleteMe
+./install.py --clangd-completer --rust-completer --java-completer
 
-echo "<<<<<< END"
+echo "let g:ycm_global_ycm_extra_conf = '~/.vim_runtime/my_plugins/YouCompleteMe/.ycm_extra_conf.py'" >> ~/.vim_runtime/my_configs.vim
